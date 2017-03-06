c395b2c8a5f811d2e012527d94ac68a6ebb38a7e
PishchalovaAgata <zemtsov.69@mail.ru>
2017-03-06 16:17:20 +0300

    rewrite Task4

--- Task4/Script4.js
+++ Task4/Script4.js
@@ -5,6 +5,7 @@ var articles = [
         summary: 'В Минске появилось и исчезло граффити Это я автор налога для безработных',
         createdAt: new Date('2017-02-27T23:00:00'),
         author: 'Автор 1',
+        tags:["minsk", "good"],
         content: 'В центре Минска появилась аппликация, на которой изображена член Совета Республики Марианна Щеткина, ранее — глава Министерства труда и социальной защиты Беларуси. Рядом с ее портретом в полный рост пояснение: «Это я автор налога для безработных». Фотографии творчества неизвестного уличного художника сделал блогер Антон Мотолько. Буквально через пару часов аппликация исчезла — ее оперативно закрасили коммунальные службы.'
     },
     {
@@ -13,6 +14,7 @@ var articles = [
         summary: 'В Литве установят сирены на случай чрезвычайных ситуаций на БелАЭС',
         createdAt: new Date('2017-01-23T23:00:00'),
         author: 'Автор 2',
+        tags:["music","belarus","plant"],
         content: 'Департамент пожарной безопасности и спасения (ДПБС) намерен оборудовать на юго-востоке Литвы предупредительные сирены, которые бы информировали население о возможной опасности в случае инцидентов на Островецкой АЭС в Беларуси, сообщает DELFI.'
     },
     {
@@ -21,6 +23,7 @@ var articles = [
         summary: 'На платформе станции метро "Институт культуры" умер мужчина',
         createdAt: new Date('2014-02-14T23:00:00'),
         author: 'Автор 1',
+        tags:["death"],
         content: 'Утром 5 марта на платформе станции 67-летнему мужчине стало плохо. Бригада из БСМП прибыла на вызов через несколько минут. Медики полчаса пытались реанимировать мужчину, но, к сожалению, в 10.30 были вынуждены констатировать смерть.'
     },
     {
@@ -29,6 +32,7 @@ var articles = [
         summary: 'Минское «Динамо» обыграло ярославский «Локомотив» в четвертом матче первого раунда плей-офф КХЛ — 4:2',
         createdAt: new Date('2017-04-27T23:00:00'),
         author: 'Иванов Иван',
+        tags:["win", "sport", "russia"],
         content: 'Гости создали больше опасных моментов и в два раза перебросали минчан, но «зубры» на этот раз очень эффективно использовали свои моменты.'
     },
     {
@@ -37,6 +41,7 @@ var articles = [
         summary: 'В Минске появилось и исчезло граффити Это я автор налога для безработных',
         createdAt: new Date('2017-02-27T23:00:00'),
         author: 'Автор 1',
+        tags:["minsk", "good"],
         content: 'В центре Минска появилась аппликация, на которой изображена член Совета Республики Марианна Щеткина, ранее — глава Министерства труда и социальной защиты Беларуси. Рядом с ее портретом в полный рост пояснение: «Это я автор налога для безработных». Фотографии творчества неизвестного уличного художника сделал блогер Антон Мотолько. Буквально через пару часов аппликация исчезла — ее оперативно закрасили коммунальные службы.'
     },
     {
@@ -45,6 +50,7 @@ var articles = [
         summary: 'В Литве установят сирены на случай чрезвычайных ситуаций на БелАЭС',
         createdAt: new Date('2017-01-23T23:00:00'),
         author: 'Автор 2',
+        tags:["music","belarus","plant"],
         content: 'Департамент пожарной безопасности и спасения (ДПБС) намерен оборудовать на юго-востоке Литвы предупредительные сирены, которые бы информировали население о возможной опасности в случае инцидентов на Островецкой АЭС в Беларуси, сообщает DELFI.'
     },
     {
@@ -53,6 +59,7 @@ var articles = [
         summary: 'На платформе станции метро "Институт культуры" умер мужчина',
         createdAt: new Date('2014-02-14T23:00:00'),
         author: 'Автор 1',
+        tags:["death"],
         content: 'Утром 5 марта на платформе станции 67-летнему мужчине стало плохо. Бригада из БСМП прибыла на вызов через несколько минут. Медики полчаса пытались реанимировать мужчину, но, к сожалению, в 10.30 были вынуждены констатировать смерть.'
     },
     {
@@ -61,6 +68,7 @@ var articles = [
         summary: 'Минское «Динамо» обыграло ярославский «Локомотив» в четвертом матче первого раунда плей-офф КХЛ — 4:2',
         createdAt: new Date('2017-04-27T23:00:00'),
         author: 'Иванов Иван',
+        tags:["win", "sport", "russia"],
         content: 'Гости создали больше опасных моментов и в два раза перебросали минчан, но «зубры» на этот раз очень эффективно использовали свои моменты.'
     },
     {
@@ -69,6 +77,7 @@ var articles = [
         summary: 'В Минске появилось и исчезло граффити Это я автор налога для безработных',
         createdAt: new Date('2017-02-27T23:00:00'),
         author: 'Автор 1',
+        tags:["minsk", "good"],
         content: 'В центре Минска появилась аппликация, на которой изображена член Совета Республики Марианна Щеткина, ранее — глава Министерства труда и социальной защиты Беларуси. Рядом с ее портретом в полный рост пояснение: «Это я автор налога для безработных». Фотографии творчества неизвестного уличного художника сделал блогер Антон Мотолько. Буквально через пару часов аппликация исчезла — ее оперативно закрасили коммунальные службы.'
     },
     {
@@ -77,6 +86,7 @@ var articles = [
         summary: 'В Литве установят сирены на случай чрезвычайных ситуаций на БелАЭС',
         createdAt: new Date('2017-01-23T23:00:00'),
         author: 'Автор 2',
+        tags:["music","belarus","plant"],
         content: 'Департамент пожарной безопасности и спасения (ДПБС) намерен оборудовать на юго-востоке Литвы предупредительные сирены, которые бы информировали население о возможной опасности в случае инцидентов на Островецкой АЭС в Беларуси, сообщает DELFI.'
     },
     {
@@ -85,6 +95,7 @@ var articles = [
         summary: 'На платформе станции метро "Институт культуры" умер мужчина',
         createdAt: new Date('2014-02-14T23:00:00'),
         author: 'Автор 1',
+        tags:["death"],
         content: 'Утром 5 марта на платформе станции 67-летнему мужчине стало плохо. Бригада из БСМП прибыла на вызов через несколько минут. Медики полчаса пытались реанимировать мужчину, но, к сожалению, в 10.30 были вынуждены констатировать смерть.'
     },
     {
@@ -93,6 +104,7 @@ var articles = [
         summary: 'Минское «Динамо» обыграло ярославский «Локомотив» в четвертом матче первого раунда плей-офф КХЛ — 4:2',
         createdAt: new Date('2017-08-27T23:00:00'),
         author: 'Иванов Иван',
+        tags:["win", "sport", "russia"],
         content: 'Гости создали больше опасных моментов и в два раза перебросали минчан, но «зубры» на этот раз очень эффективно использовали свои моменты.'
     },
     {
@@ -101,6 +113,7 @@ var articles = [
         summary: 'В Минске появилось и исчезло граффити Это я автор налога для безработных',
         createdAt: new Date('2017-02-27T23:00:00'),
         author: 'Любава',
+        tags:["minsk", "good"],
         content: 'В центре Минска появилась аппликация, на которой изображена член Совета Республики Марианна Щеткина, ранее — глава Министерства труда и социальной защиты Беларуси. Рядом с ее портретом в полный рост пояснение: «Это я автор налога для безработных». Фотографии творчества неизвестного уличного художника сделал блогер Антон Мотолько. Буквально через пару часов аппликация исчезла — ее оперативно закрасили коммунальные службы.'
     },
     {
@@ -109,6 +122,7 @@ var articles = [
         summary: 'В Литве установят сирены на случай чрезвычайных ситуаций на БелАЭС',
         createdAt: new Date('2017-08-23T23:00:00'),
         author: 'Автор 2',
+        tags:["music","belarus","plant"],
         content: 'Департамент пожарной безопасности и спасения (ДПБС) намерен оборудовать на юго-востоке Литвы предупредительные сирены, которые бы информировали население о возможной опасности в случае инцидентов на Островецкой АЭС в Беларуси, сообщает DELFI.'
     },
     {
@@ -117,6 +131,7 @@ var articles = [
         summary: 'На платформе станции метро "Институт культуры" умер мужчина',
         createdAt: new Date('2014-02-14T23:00:00'),
         author: 'Виталий Рок',
+        tags:["death"],
         content: 'Утром 5 марта на платформе станции 67-летнему мужчине стало плохо. Бригада из БСМП прибыла на вызов через несколько минут. Медики полчаса пытались реанимировать мужчину, но, к сожалению, в 10.30 были вынуждены констатировать смерть.'
     },
     {
@@ -125,6 +140,7 @@ var articles = [
         summary: 'Минское «Динамо» обыграло ярославский «Локомотив» в четвертом матче первого раунда плей-офф КХЛ — 4:2',
         createdAt: new Date('2016-07-28T23:00:00'),
         author: 'Светлана Алексеева',
+        tags:["win", "sport", "russia"],
         content: 'Гости создали больше опасных моментов и в два раза перебросали минчан, но «зубры» на этот раз очень эффективно использовали свои моменты.'
     },
     {
@@ -133,6 +149,7 @@ var articles = [
         summary: 'В Минске появилось и исчезло граффити Это я автор налога для безработных',
         createdAt: new Date('2017-02-27T23:00:00'),
         author: 'Алексей Алексеев',
+        tags:["minsk", "good"],
         content: 'В центре Минска появилась аппликация, на которой изображена член Совета Республики Марианна Щеткина, ранее — глава Министерства труда и социальной защиты Беларуси. Рядом с ее портретом в полный рост пояснение: «Это я автор налога для безработных». Фотографии творчества неизвестного уличного художника сделал блогер Антон Мотолько. Буквально через пару часов аппликация исчезла — ее оперативно закрасили коммунальные службы.'
     },
     {
@@ -141,6 +158,7 @@ var articles = [
         summary: 'В Литве установят сирены на случай чрезвычайных ситуаций на БелАЭС',
         createdAt: new Date('2014-01-47T23:00:00'),
         author: 'Снежок',
+        tags:["music","belarus","plant"],
         content: 'Департамент пожарной безопасности и спасения (ДПБС) намерен оборудовать на юго-востоке Литвы предупредительные сирены, которые бы информировали население о возможной опасности в случае инцидентов на Островецкой АЭС в Беларуси, сообщает DELFI.'
     },
     {
@@ -149,6 +167,7 @@ var articles = [
         summary: 'На платформе станции метро "Институт культуры" умер мужчина',
         createdAt: new Date('2012-07-14T23:00:00'),
         author: 'Игорь Игоревич',
+        tags:["death"],
         content: 'Утром 5 марта на платформе станции 67-летнему мужчине стало плохо. Бригада из БСМП прибыла на вызов через несколько минут. Медики полчаса пытались реанимировать мужчину, но, к сожалению, в 10.30 были вынуждены констатировать смерть.'
     },
     {
@@ -157,9 +176,13 @@ var articles = [
         summary: 'Минское «Динамо» обыграло ярославский «Локомотив» в четвертом матче первого раунда плей-офф КХЛ — 4:2',
         createdAt: new Date('2017-04-27T23:00:00'),
         author: 'Иванов Иван',
+        tags:["win", "sport", "russia"],
         content: 'Гости создали больше опасных моментов и в два раза перебросали минчан, но «зубры» на этот раз очень эффективно использовали свои моменты.'
     }
 ];
+
+
+var tags=["good","music","minsk","russia","belarus","plant","sport","win", "death", 'uuuuu', 'newNews'];
 
 function getArticles(skip,top,filterConfiguration){
     skip = skip || 0;
@@ -168,7 +191,7 @@ function getArticles(skip,top,filterConfiguration){
         var _author = filterConfiguration.author || "";
         var _dateBegin = filterConfiguration.dateBegin || new Date(-8640000000000000);
         var _dateEnd = filterConfiguration.dateEnd || new Date(8640000000000000);
-        var _tagsArray = filterConfiguration.tags;
+        var _tags = filterConfiguration.tags || [];
     }
     var _articles = articles;
     if(filterConfiguration!==undefined) {
@@ -179,6 +202,21 @@ function getArticles(skip,top,filterConfiguration){
             return a.createdAt >= _dateBegin && a.createdAt <= _dateEnd;
         });
     }
+
+    if(_tags !== undefined) {
+        _articles = _articles.filter(function (a) {
+            var length =_tags.length;
+            var count = 0;
+            for (var i = 0; i < a.tags.length; i++) {
+                for (var j = 0; j < _tags.length; j++) {
+                    if (a.tags[i] === _tags[j]) count++;
+                }
+            }
+            if (count === length) return true;
+            return false;
+        });
+    }
+
     _articles = _articles.slice(skip, skip + top);
     return _articles.sort(function (a, b) {
         if(a.createdAt - b.createdAt < 0){
@@ -210,6 +248,17 @@ function validateArticle(article) {
         return false;
     if(typeof article.id !== 'string' || article.id.length <= 0 || getArticle(article.id) !== undefined)
         return false;
+    if( article.tags === undefined)
+        return false;
+    var count = 0;
+    for(var i=0; i < article.tags.length ;i++){
+        for(var j=0;j<tags.length;j++){
+            if(tags[j] === article.tags[i])
+                count++;
+        }
+    }
+    if(count !== article.tags.length)
+        return false;
     return true;
 };
 
@@ -224,10 +273,23 @@ function validateArticleForEdit(article) {
         return false;
     if(typeof article.content !== 'string' || article.content.length <= 0)
         return false;
+    if(article.tags.length<=0)
+        return false;
+    if( article.tags === undefined)
+        return false;
+    var count = 0;
+    for(var i=0; i < article.tags.length ;i++){
+        for(var j=0;j<tags.length;j++){
+            if(tags[j] === article.tags[i])
+                count++;
+        }
+    }
+    if(count !== article.tags.length)
+        return false;
     return true;
 };
 function addArticle(article) {
-    if(validateArticle(article)===false){
+    if(validateArticle(article)=== false){
         return false;
     }
     articles.push(article);
@@ -236,17 +298,21 @@ function addArticle(article) {
 
 function editArticle(id, article) {
     var _article;
-    if((_article=getArticle(id))===undefined)return false;
+    if((_article = getArticle(id)) === undefined)
+        return false;
     if(article.title !== undefined)
         _article.title=article.title;
     if(article.summary !== undefined)
         _article.summary=article.summary;
     if(article.content !== undefined)
         _article.content=article.content;
+    if(article.tags!==undefined)
+        _article.tags=article.tags;
     if(validateArticleForEdit(_article) === true){
         getArticle(id).title=_article.title;
         getArticle(id).summary=_article.summary;
         getArticle(id).content=_article.content;
+        getArticle(id).tags=_article.tags;
         return true;
     }
     return false;
@@ -260,6 +326,30 @@ function removeArticle(id) {
     if (index !== undefined){
     articles.splice(index, 1);
     return true;
+    }
+    return false;
+};
+
+function addTag(tag) {
+    if(inTags(tag) === -1) {
+        tags.push(tag);
+        return true;
+    }
+    return false;
+};
+
+function inTags(tag) {
+    for(var i=0;i<tags.length;i++){
+        if(tags[i] === tag) return i;
+    }
+    return -1;
+};
+
+function deleteTag(tag) {
+    var _extraTag =inTags(tag);
+    if(_extraTag !== -1) {
+        tags.splice(_extraTag, 1);
+        return true;
     }
     return false;
 };
@@ -279,6 +369,9 @@ console.log(getArticles(1,9,{author:'Автор 1',beginDate:new Date('2015-01-2
 console.log("*************************");
 console.log("Вывод из промежутка от 0 до 5 с 'пустым' фильтром");
 console.log(getArticles(0,5,{}));
+console.log("*************************");
+console.log("Вывод с фильтрацией по тегу 'russia':");
+console.log(getArticles(0,9,{tags: ['russia']}));
 
 console.log("\n\r----------getArticle:----------");
 console.log("Поиск статьи по существующему id");
@@ -292,22 +385,30 @@ console.log(getArticle());
 
 console.log("\n\r----------addArticle:----------");
 console.log("пример добавления новости, соответствующей всем параметрам валидности ");
-console.log(addArticle({title:"Новость 21",id:"21",summary:"Кратко 21",author:"Автор 21",createdAt:new Date(),content:"Контент 21"}));
+console.log(addArticle({title:"Новость 21",id:"21",summary:"Кратко 21",author:"Автор 21",tags: ['russia', 'music'],createdAt:new Date(),content:"Контент 21"}));
+console.log("*************************");
 console.log("Добавление новости с уже существующим id ");
-console.log(addArticle({title:"Новость 7",id:"2",summary:"Кратко 7",author:"Автор 7",createdAt:new Date(),content:"Контент 7"}));
+console.log(addArticle({title:"Новость 7",id:"2",summary:"Кратко 7",author:"Автор 7",createdAt:new Date(), tags: ['insta'],content:"Контент 7"}));
+console.log("*************************");
 console.log("Добавление новости, в которой пользователь указал пустым одно из обязательных полей ");
-console.log(addArticle({title:"Новость 8",id:"8",summary:"Кратко 8",author:"Автор 8",createdAt:new Date(),content:""}));
+console.log(addArticle({title:"Новость 28",id:"28",summary:"Кратко 28",author:"Автор 28",tags:[], createdAt:new Date(),content:""}));
+console.log("*************************");
+console.log("Добавление новости, в которой пользователь указал тег, не входящий в массив тегов ");
+console.log(addArticle({title:"Новость 25",id:"20",summary:"Кратко 25",author:"Автор 8",tags:['russia', 'english', 'bear'], createdAt:new Date(),content:""}));
 
 
 console.log("\n\r----------editArticle:----------");
 console.log("Изменение Заголовка в первой новости ");
 console.log(editArticle("1",{title:"Новая новость 1"}));
 console.log(getArticle("1"));
+console.log("*************************");
 console.log("Изменение новости, id которой не существует");
 console.log(editArticle("666",{title:"Новое название"}));
+console.log("*************************");
 console.log("Полное преображение новости 2");
-console.log(editArticle("2",{title:"Новая Новость 2",summary: "Новое кратко 2", content:"Новый контент 2"}));
+console.log(editArticle("2",{title:"Новая Новость 2",summary: "Новое кратко 2", content:"Новый контент 2", tags:['uuuuu', 'newNews']}));
 console.log(getArticle("2"));
+console.log("*************************");
 console.log("Изменение новости, когда задали одно из полей пустым");
 console.log(editArticle("3",{title:"Новая новость 3",content:""}));
 
@@ -315,5 +416,21 @@ console.log("\n\r----------removeArticle:----------");
 console.log("Удаление элемента с id = 4 ");
 console.log(removeArticle("4"));
 console.log(getArticles());
+console.log("*************************");
 console.log("Удаление элемента с несуществующим id");
 console.log(removeArticle("132"));
+
+
+console.log("\n\r----------addTag:----------");
+console.log("Пример добавления не существующего тега");
+console.log(addTag('belavia'));
+console.log("*************************");
+console.log("Попытка добавить существующий тег");
+console.log(addTag('music'));
+
+console.log("\n\r----------deleteTag:----------");
+console.log("Удаление существующего тега");
+console.log(deleteTag('plant'));
+console.log("*************************");
+console.log("Удаление несуществующего тега");
+console.log(deleteTag('Lokomotiv'));
