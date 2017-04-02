21de3407d36c01fffdb358fac7b917394231909a
PishchalovaAgata <zemtsov.69@mail.ru>
2017-04-03 00:02:09 +0300

    add Task8

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
-var express =require("express");
+var express =require("express"),
+    fs = require("fs"),
+    bodyParser = require("body-parser"),
+    diskdb = require("diskdb");
 var app = express();
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function (request,response) {
-    response.sendfile('public/index.html');
-
-})
+    response.sendfile('index.html');
+});
 app.listen(3000);
+var articles,arrayIMG, Users;
+diskdb.connect(__dirname + "/public/Json", ["articles", "arrayIMG", "Users"]);
+
+var user = null;
+
+Users = diskdb.Users.find();
+app.post("/logIn", function (require, response) {
+    var actualUser;
+    if(actualUser = Users.find(p => p.username === require.body.username && p.password === require.body.password)){
+        user = actualUser.username;
+    }
+});
+
+app.post("/logOut", function (require, response) {
+    user = null;
+});
+
+app.get("/actualUser", function (require, response) {
+    if(user === null){
+        response.status(400).send("Current user is not existed!");
+    }
+    else{
+        response.send(user);
+    }
+});
+
+
+
+app.get("/articles", (require, response)=> {response.send(diskdb.articles.find());});
+app.post("/articles", (require,response)=> {
+    diskdb.articles.remove();
+    diskdb.loadCollections(["articles"]);
+    diskdb.articles.save(require.body);
+});
+
+app.get("/arrayIMG", (require, response)=> {response.send(diskdb.arrayIMG.find());});
+app.post("/arrayIMG", (require,response)=> {
+    diskdb.arrayIMG.remove();
+    diskdb.loadCollections(["arrayIMG"]);
+    diskdb.arrayIMG.save(require.body);
+});
+
+
+
+
+
+
+
